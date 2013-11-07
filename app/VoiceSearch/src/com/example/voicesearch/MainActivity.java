68151b723eceaa9b6e5ac3d4f21fa7918bbfd8d0
anantag <anantag23@gmail.com>
2013-11-07 09:48:13 -0500

    Added a new flight search app and made some changes to the previous voice search app

--- app/VoiceSearch/src/com/example/voicesearch/MainActivity.java
+++ app/VoiceSearch/src/com/example/voicesearch/MainActivity.java
@@ -26,13 +26,15 @@ public class MainActivity extends Activity implements OnClickListener, OnInitLis
 	    private ListView wordsList;
 	    private TextToSpeech tts;
 	    ArrayList <String> matches;
-	 
+	    boolean [] flagVar = new boolean[10];
 	    /**
 	     * Called with the activity is first created.
 	     */
 	    @Override
 	    public void onCreate(Bundle savedInstanceState)
 	    {
+	    	for(int i=0;i<10;i++)
+	    		flagVar[i] = false;
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.activity_main);
 	        
@@ -59,7 +61,20 @@ public class MainActivity extends Activity implements OnClickListener, OnInitLis
 	     */
 	    public void speakButtonClicked(View v)
 	    {
-	        startVoiceRecognitionActivity();
+	        //tts.stop();
+	        //tts.shutdown();
+			if(flagVar[0] == false){
+				String test="";
+				test+=":";
+				test+="Welcome to Flight Search!!";
+				test+=":";
+				test+="Where do you want to fly to?";
+				if(test!=null){
+					tts.speak(test, TextToSpeech.QUEUE_FLUSH,null);				
+				}
+			}
+			flagVar[0]=true;	      
+			startVoiceRecognitionActivity();
 	    }
 	 
 	    /**
@@ -67,9 +82,15 @@ public class MainActivity extends Activity implements OnClickListener, OnInitLis
 	     */
 	    private void startVoiceRecognitionActivity()
 	    {
-	        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+	    	boolean speakingEnd = tts.isSpeaking();
+	    	
+			do{
+			   speakingEnd = tts.isSpeaking();
+			} while (speakingEnd);	
+			
+	    	Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 	        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-	        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice recognition Demo...");
+	        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Flight Search");
 	        startActivityForResult(intent, REQUEST_CODE);
 	    }
 	 
@@ -103,21 +124,57 @@ public class MainActivity extends Activity implements OnClickListener, OnInitLis
 		@Override
 		public void onClick(View v) {
 			if(tts!=null){
-				String test = "";
-				for(int i=0;i<matches.size();i++){
-					test+=":";
-					test += matches.get(i).toString();
-					test+=":";
-				}
-//				tts.speak(test, TextToSpeech.QUEUE_FLUSH, null);
-				//String text = matches.get(0);
+				if(flagVar[0]==true && flagVar[1]==false){
+					String test = "";
+						test+=":";
+						test+="You want to fly to ";
+						test += matches.get(0).toString();
+						test+=":";
+						test+=":";
+						test+=":";
+						test+="Is that correct?";
+						test+=":";
+						test+=":";
+						test+="Please say yes or no";
 					if(test!=null){
-							if(!tts.isSpeaking()){
-									tts.speak(test, TextToSpeech.QUEUE_FLUSH,null);
-								}							
+						if(!tts.isSpeaking()){
+							tts.speak(test, TextToSpeech.QUEUE_FLUSH,null);
+						}							
 					}
 				}
+				flagVar[1]=true;
+				
+				boolean speakingEnd = tts.isSpeaking();
+				do{
+				   speakingEnd = tts.isSpeaking();
+				} while (speakingEnd);	
+				
+				startVoiceRecognitionActivity();
+				
+				try{
+					Thread.sleep(6000);
+				}catch(InterruptedException e){
+					System.out.println("Error");
+				}
+				
+				if(flagVar[0]==true && flagVar[1]==true && flagVar[2]==false && matches.get(0).toString()=="yes"){
+					String test = "";
+					//for(int i=0;i<matches.size();i++){
+						test+=":";
+						test+="Where do you want to fly from ";
+						test+=":";
+						test+=":";					//}
+	//				tts.speak(test, TextToSpeech.QUEUE_FLUSH, null);
+					//String text = matches.get(0);
+					if(test!=null){
+						if(!tts.isSpeaking()){
+							tts.speak(test, TextToSpeech.QUEUE_FLUSH,null);
+						}							
+					}
+				}
+				flagVar[2]=true;
 			}
+		}
 		
 		protected void onDestroy(){
 			if(tts!=null){
